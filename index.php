db42213920e16942cb21dabdc8a92ddd6c4fa157
Nik <Nik@MacBook-Air-Admin.local>
2016-12-10 23:13:38 +0200

    Step 5: Separate PHP Logic From Presentation

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <style>
-        header{
-            background: #e3e3e3;
-            padding: 2em;
-            text-align: center;
-        }
-    </style>
-</head>
-<body>
-    <header>
-        <h1><?= 'Hello, '.htmlspecialchars($_GET['name']); ?></h1>
-    </header>
-</body>
-</html>
+<?php
+
+$greeting = 'Hello, '.htmlspecialchars($_GET['name']);
+
+require 'index.view.php';
