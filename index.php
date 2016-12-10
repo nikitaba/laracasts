5893033bf3aae9b05e2242f81adb0d73f1490469
Nik <Nik@MacBook-Air-Admin.local>
2016-12-11 00:10:36 +0200

    Step 7: Associative Arrays

--- index.php
+++ index.php
@@ -1,11 +1,20 @@
 <?php
 
-$animals = [
-    'cat',
-    'dog',
-    'cow',
-    'rabbit',
-    'motherfucker'
+//Initializing new array
+$person = [
+    'age' => 25,
+    'hair' => 'brown',
+    'career' => 'web developer'
 ];
+
+//Pushing new item with an index 'name' to the array
+$person['name'] = 'Nik'; //We would use $person[] = 'Nik' for a regular array
+
+//Removing age item from the array
+unset($person['age']);
+
+/*
+ * die(var_dump($person));
+ */
 
 require 'index.view.php';
--- index.view.php
+++ index.view.php
@@ -7,8 +7,8 @@
 <body>
     <header>
         <ul>
-            <?php foreach ($animals as $animal): ?>
-                <li><?= $animal; ?></li>
+            <?php foreach ($person as $feature => $value): ?>
+                <li><?= '<strong>'.$feature.': </strong>'.$value; ?></li>
             <?php endforeach; ?>
         </ul>
     </header>
