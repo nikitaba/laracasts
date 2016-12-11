7f1cfa08ef5f521127a4fe90434eb78d3fbde699
Nik <Nik@MacBook-Air-Admin.local>
2016-12-11 15:31:42 +0200

    Step 15: Hide Your Secret Passwords

--- database/Connection.php
+++ database/Connection.php
@@ -2,10 +2,15 @@
 
 class Connection
 {
-    public static function make()
+    public static function make($config)
     {
         try {
-            return new PDO('mysql:host=127.0.0.1;dbname=laravel', 'root', 'root');
+//            return new PDO('mysql:host=127.0.0.1;dbname=laravel', 'root', 'root');
+            return new PDO(
+                $config['connection'].';dbname='.$config['dbname'],
+                $config['username'],
+                $config['password']
+            );
         } catch (PDOException $e) {
             die($e->getMessage());
         }
